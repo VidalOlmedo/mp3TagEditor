5682df2cc6c58f0c2e81885859d057282baa0170
VidalOlmedo <vidalolmedo@outlook.com>
2020-08-18 21:40:59 -0500

    .

--- mp3TagEditor/Program.cs
+++ mp3TagEditor/Program.cs
@@ -1,55 +1,92 @@
 ﻿using System;
-using TagLib;
+//using TagLib;
 using System.IO;
 
 namespace mp3TagEditor
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] paths)
         {
-            /*File file = File.Create("C:/Users/Vidal Olmedo/Desktop/pruebasmp3/Alien Nation.mp3");
-
-            //file.Tag.Title = "";
-
-            file.Save();
-
-            Console.WriteLine($"AUTOR: {file.Tag.Performers[0]}");
-            Console.WriteLine($"TITLE: {file.Tag.Title}");
-
-            string input = Console.ReadLine();
-
-            Console.WriteLine(input);
-            Console.WriteLine($"LENGTH: {input.Length}");*/
-
-
-
             //args[0] = source folder (as copied from the route in the file explorer)
             //args[1] = destination folder
 
-            /*for(int i = 0; i < args.Length; i++)
+            if(paths.Length == 0)
             {
-                Console.WriteLine(args[i]);
-            }*/
+                paths = new string[2];
+                paths[0] = "C:/Users/Vidal Olmedo/Music/Music"; //source
+                paths[1] = "C:/Users/Vidal Olmedo/Music/MUSIC LISTOS";  //destination
+            }
 
-            string[] files = Directory.GetFiles(args[0]);
-            int pathLength = args[0].Length + 1; //+1 for the ending slash
+            string[] files = Directory.GetFiles(paths[0]);
+            int pathLength = paths[0].Length + 1; //+1 for the ending slash
 
-            string fileName;
+            string fileName, newName, performer, newPerformer;
             int extensionIndex;
+
+            TagLib.File file;
 
             for(int i = 0; i < files.Length; i++)
             {
+                Console.Clear();
+
                 fileName = files[i].Substring(pathLength);
-
                 extensionIndex = fileName.LastIndexOf(".");
-
                 fileName = fileName.Remove(extensionIndex);
 
-                Console.WriteLine($"*NOMBRE* {fileName}");
+                file = TagLib.File.Create(files[i]);
+
+
+                if(file.Tag.Performers.Length == 0)
+                {
+                    performer = "-";
+                }
+                else
+                {
+                    performer = file.Tag.Performers[0];
+                }
+
+                Console.WriteLine($"*NOMBRE* {fileName} *ARTISTA* {performer}");
+
+                Console.Write("Nombre: ");
+                newName = Console.ReadLine();
+
+                if(newName == "")
+                {
+                    newName = fileName;
+                }
+
+                Console.Write("Artista: ");
+                GetPerformer();
+
+                string[] performersArray = { newPerformer };
+
+                file.Tag.Performers = performersArray;
+
+                file.Save();
+
+                Directory.Move(files[i], paths[1] + "/" + newName + ".mp3");
             }
 
+            Console.Clear();
+            Console.WriteLine("No hay mas archivos");
 
+            void GetPerformer()
+            {                
+                newPerformer = Console.ReadLine();
+
+                if (newPerformer == "")
+                {
+                    if (performer == "-")
+                    {
+                        GetPerformer();
+                    }
+                    else
+                    {
+                        newPerformer = performer;
+                    }                 
+                }
+            }
         }
     }
 }
